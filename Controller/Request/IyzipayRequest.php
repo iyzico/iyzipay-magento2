8ab8ab607b89a55e4025e55e7b5d809e3f837301
Tarık KAMAT <tarikkamat2@gmail.com>
2024-12-06 11:31:47 +0300

    IyzipayRequest Exception Handling

--- Controller/Request/IyzipayRequest.php
+++ Controller/Request/IyzipayRequest.php
@@ -41,6 +41,7 @@ use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Quote\Api\CartManagementInterface;
 use Magento\Quote\Api\CartRepositoryInterface;
 use Magento\Quote\Model\Quote;
+use Exception;
 
 
 class IyzipayRequest implements ActionInterface
@@ -72,94 +73,102 @@ class IyzipayRequest implements ActionInterface
      */
     public function execute(): Json
     {
-        // Get the configuration values
-        $apiKey = $this->configHelper->getApiKey();
-        $secretKey = $this->configHelper->getSecretKey();
-        $baseUrl = $this->configHelper->getBaseUrl();
-        $callbackUrl = $this->configHelper->getCallbackUrl();
-        $paymentSource = $this->configHelper->getPaymentSource();
-        $locale = $this->configHelper->getLocale();
-        $currency = $this->configHelper->getCurrency();
+        try {
+            // Get the configuration values
+            $apiKey = $this->configHelper->getApiKey();
+            $secretKey = $this->configHelper->getSecretKey();
+            $baseUrl = $this->configHelper->getBaseUrl();
+            $callbackUrl = $this->configHelper->getCallbackUrl();
+            $paymentSource = $this->configHelper->getPaymentSource();
+            $locale = $this->configHelper->getLocale();
+            $currency = $this->configHelper->getCurrency();
 
-        // Ensure the cookies are same site
-        $this->utilityHelper->ensureCookiesSameSite();
-        $resultJson = $this->resultJsonFactory->create();
-        $checkoutSession = $this->checkoutSession->getQuote();
+            // Ensure the cookies are same site
+            $this->utilityHelper->ensureCookiesSameSite();
+            $resultJson = $this->resultJsonFactory->create();
+            $checkoutSession = $this->checkoutSession->getQuote();
 
-        // Configure the buyer
-        $customerId = $this->utilityHelper->getCustomerId($this->customerSession);
-        $cardUserKey = $this->utilityHelper->getCustomerCardUserKey($this->iyziCardFactory, $customerId, $apiKey);
-        $buyer = $this->objectHelper->createBuyer($checkoutSession);
+            // Configure the buyer
+            $customerId = $this->utilityHelper->getCustomerId($this->customerSession);
+            $cardUserKey = $this->utilityHelper->getCustomerCardUserKey($this->iyziCardFactory, $customerId, $apiKey);
+            $buyer = $this->objectHelper->createBuyer($checkoutSession);
 
-        // Configure the basket
-        $basketId = $this->checkoutSession->getQuoteId();
-        $conversationId = $this->utilityHelper->generateConversationId($basketId);
-        $basketItems = $this->objectHelper->createBasketItems($checkoutSession);
+            // Configure the basket
+            $basketId = $this->checkoutSession->getQuoteId();
+            $conversationId = $this->utilityHelper->generateConversationId($basketId);
+            $basketItems = $this->objectHelper->createBasketItems($checkoutSession);
 
-        // Configure the price
-        $price = $this->utilityHelper->calculateSubtotalPrice($checkoutSession);
-        $paidPrice = $this->utilityHelper->parsePrice(round($checkoutSession->getGrandTotal(), 2));
+            // Configure the price
+            $price = $this->utilityHelper->calculateSubtotalPrice($checkoutSession);
+            $paidPrice = $this->utilityHelper->parsePrice(round($checkoutSession->getGrandTotal(), 2));
 
-        // Configure the address
-        $shippingAddress = $this->objectHelper->createShippingAddress($checkoutSession);
-        $billingAddress = $this->objectHelper->createBillingAddress($checkoutSession);
+            // Configure the address
+            $shippingAddress = $this->objectHelper->createShippingAddress($checkoutSession);
+            $billingAddress = $this->objectHelper->createBillingAddress($checkoutSession);
 
-        // Create the request
-        $request = new CreateCheckoutFormInitializeRequest();
-        $request->setLocale($locale);
-        $request->setConversationId($conversationId);
-        $request->setPrice($price);
-        $request->setPaidPrice($paidPrice);
-        $request->setCurrency($currency);
-        $request->setBasketId($basketId);
-        $request->setPaymentGroup("PRODUCT");
-        $request->setCallbackUrl($callbackUrl);
-        $request->setPaymentSource($paymentSource);
-        $request->setBuyer($buyer);
-        $request->setShippingAddress($shippingAddress);
-        $request->setBillingAddress($billingAddress);
-        $request->setBasketItems($basketItems);
-        $request->setCardUserKey($cardUserKey);
-        $request->setGoBackUrl($this->configHelper->getGoBackUrl());
+            // Create the request
+            $request = new CreateCheckoutFormInitializeRequest();
+            $request->setLocale($locale);
+            $request->setConversationId($conversationId);
+            $request->setPrice($price);
+            $request->setPaidPrice($paidPrice);
+            $request->setCurrency($currency);
+            $request->setBasketId($basketId);
+            $request->setPaymentGroup("PRODUCT");
+            $request->setCallbackUrl($callbackUrl);
+            $request->setPaymentSource($paymentSource);
+            $request->setBuyer($buyer);
+            $request->setShippingAddress($shippingAddress);
+            $request->setBillingAddress($billingAddress);
+            $request->setBasketItems($basketItems);
+            $request->setCardUserKey($cardUserKey);
+            $request->setGoBackUrl($this->configHelper->getGoBackUrl());
 
-        // Create the options
-        $options = new Options();
-        $options->setBaseUrl($baseUrl);
-        $options->setApiKey($apiKey);
-        $options->setSecretKey($secretKey);
+            // Create the options
+            $options = new Options();
+            $options->setBaseUrl($baseUrl);
+            $options->setApiKey($apiKey);
+            $options->setSecretKey($secretKey);
 
-        $response = CheckoutFormInitialize::create($request, $options);
+            $response = CheckoutFormInitialize::create($request, $options);
 
-        $responseConversationId = $response->getConversationId();
-        $responseToken = $response->getToken();
-        $responseSignature = $response->getSignature();
+            $responseConversationId = $response->getConversationId();
+            $responseToken = $response->getToken();
+            $responseSignature = $response->getSignature();
 
-        $calculateSignature = $this->utilityHelper->calculateHmacSHA256Signature([
-            $responseConversationId,
-            $responseToken
-        ], $secretKey);
+            $calculateSignature = $this->utilityHelper->calculateHmacSHA256Signature([
+                $responseConversationId,
+                $responseToken
+            ], $secretKey);
 
-        if ($responseSignature === $calculateSignature) {
-            $this->utilityHelper->storeSessionData($checkoutSession, $this->customerSession);
+            if ($responseSignature === $calculateSignature) {
+                $this->utilityHelper->storeSessionData($checkoutSession, $this->customerSession);
 
-            $oldOrderId = $this->orderJobService->findOrderIdByQuoteId($basketId);
+                $oldOrderId = $this->orderJobService->findOrderIdByQuoteId($basketId);
 
-            if ($oldOrderId) {
-                $this->orderService->cancelOrder($oldOrderId);
+                if ($oldOrderId) {
+                    $this->orderService->cancelOrder($oldOrderId);
+                }
+
+                $orderId = $this->orderService->placeOrder($basketId, $this->customerSession, $this->cartManagement);
+                $this->orderJobService->saveIyziOrderJobTable($response, $basketId, $orderId);
+                return $resultJson->setData([
+                    'success' => true,
+                    'url' => $response->getPaymentPageUrl()
+                ]);
             }
 
-            $orderId = $this->orderService->placeOrder($basketId, $this->customerSession, $this->cartManagement);
-            $this->orderJobService->saveIyziOrderJobTable($response, $basketId, $orderId);
             return $resultJson->setData([
-                'success' => true,
-                'url' => $response->getPaymentPageUrl()
+                'success' => false,
+                'message' => "Signature Mismatch",
+                'code' => "0"
+            ]);
+        } catch (Exception $e) {
+            return $this->resultJsonFactory->create()->setData([
+                'success' => false,
+                'message' => $e->getMessage(),
+                'code' => $e->getCode()
             ]);
         }
-
-        return $resultJson->setData([
-            'success' => false,
-            'message' => "Signature Mismatch",
-            'code' => "0"
-        ]);
     }
 }
