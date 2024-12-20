edb8474ea8f1776c1d65bdd337b87e5318c8b9ad
Tarık KAMAT <tarikkamat2@gmail.com>
2024-12-20 21:32:20 +0300

    Refactor Subtotal class to improve code readability and consistency; update parameter type hints and remove unnecessary whitespace

--- Model/Order/Creditmemo/Total/Subtotal.php
+++ Model/Order/Creditmemo/Total/Subtotal.php
@@ -3,34 +3,37 @@
 /**
  * iyzico Payment Gateway For Magento 2
  * Copyright (C) 2018 iyzico
- * 
+ *
  * This file is part of Iyzico/Iyzipay.
- * 
+ *
  * Iyzico/Iyzipay is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace Iyzico\Iyzipay\Model\Order\Creditmemo\Total;
 
-class Subtotal extends \Magento\Sales\Model\Order\Total\AbstractTotal
+use Magento\Sales\Model\Order\Total\AbstractTotal;
+use Magento\Sales\Model\Order\Creditmemo;
+
+class Subtotal extends AbstractTotal
 {
     /**
      * Collect Creditmemo subtotal
      *
-     * @param \Magento\Sales\Model\Order\Creditmemo $creditmemo
+     * @param Creditmemo $creditmemo
      * @return $this
      */
-    public function collect(\Magento\Sales\Model\Order\Creditmemo $creditmemo)
+    public function collect(Creditmemo $creditmemo)
     {
 
         $subtotal = 0;
@@ -39,7 +42,7 @@ class Subtotal extends \Magento\Sales\Model\Order\Total\AbstractTotal
         $baseSubtotalInclTax = 0;
 
         foreach ($creditmemo->getAllItems() as $item) {
-            
+
             if ($item->getOrderItem()->isDummy()) {
                 continue;
             }
@@ -68,7 +71,7 @@ class Subtotal extends \Magento\Sales\Model\Order\Total\AbstractTotal
 
         $creditmemo->setGrandTotal($creditmemo->getGrandTotal() + $subtotal);
         $creditmemo->setBaseGrandTotal($creditmemo->getBaseGrandTotal() + $baseSubtotal);
-        
+
 
         return $this;
     }
