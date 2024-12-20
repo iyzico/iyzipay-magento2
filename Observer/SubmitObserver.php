5c542ec8a0701d86c99b26facbb2e960e23b7e36
Tarık KAMAT <tarikkamat2@gmail.com>
2024-12-20 18:34:55 +0300

    Add SubmitObserver to disable order email sending for iyzipay payment method

---
+++ Observer/SubmitObserver.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * iyzico Payment Gateway For Magento 2
+ * Copyright (C) 2018 iyzico
+ *
+ * This file is part of Iyzico/Iyzipay.
+ *
+ * Iyzico/Iyzipay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Iyzico\Iyzipay\Observer;
+
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Sales\Model\Order;
+
+class SubmitObserver implements ObserverInterface
+{
+    /**
+     * Execute observer
+     *
+     * This method is called when the event specified in the events.xml file is triggered.
+     *
+     * @param  Observer  $observer
+     * @return void
+     */
+    public function execute(Observer $observer): void
+    {
+        /** @var  Order $order */
+        $order = $observer->getEvent()->getOrder();
+
+        $orderState = $order->getState();
+        $paymentMethod = $order->getPayment()->getMethod();
+        $blockedMethods = ['iyzipay'];
+        $blockedOrderStates = ['new', 'pending_payment', 'received'];
+
+        if (
+            in_array($paymentMethod, $blockedMethods, true) &&
+            in_array($orderState, $blockedOrderStates, true)
+        ) {
+            $order->setCanSendNewEmailFlag(false);
+            return;
+        }
+    }
+}
