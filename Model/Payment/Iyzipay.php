78e51e232c1de599b34d2bb3e96ed8379c995cc2
Tarık KAMAT <tarikkamat2@gmail.com>
2025-02-28 16:09:02 +0300

    +Update plugin version to 2.1.4 in multiple files

--- Model/Payment/Iyzipay.php
+++ Model/Payment/Iyzipay.php
@@ -23,7 +23,7 @@ namespace Iyzico\Iyzipay\Model\Payment;
 
 class Iyzipay extends \Magento\Payment\Model\Method\AbstractMethod
 {
-    const PLUGIN_VERSION = '1.8.0';
+    const PLUGIN_VERSION = '2.1.4';
     protected $_code = "iyzipay";
     protected $_isOffline = true;
 
