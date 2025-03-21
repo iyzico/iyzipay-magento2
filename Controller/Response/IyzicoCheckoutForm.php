21614d264f456c56cd7da4233fcc6e5903e3a813
Tarık KAMAT <54081414+tarikkamat@users.noreply.github.com>
2025-03-21 14:47:43 +0300

    Update IyzicoCheckoutForm.php
    Signed-off-by: Tarık KAMAT <54081414+tarikkamat@users.noreply.github.com>

--- Controller/Response/IyzicoCheckoutForm.php
+++ Controller/Response/IyzicoCheckoutForm.php
@@ -259,8 +259,6 @@ class IyzicoCheckoutForm extends Action implements CsrfAwareActionInterface
             $iyziOrderModel->setData('order_id', $response->getBasketId());
             $iyziOrderModel->setData('status', $status);
             $iyziOrderModel->save();
-            $tableName = $resource->getTableName('iyzico_order');
-            $connection->insert($tableName, $iyziOrderModel->getData());
 
             /*Bank Transfer */
             if ($response->getPaymentStatus() == 'INIT_BANK_TRANSFER' && $response->getStatus() == 'success') {
@@ -346,8 +344,6 @@ class IyzicoCheckoutForm extends Action implements CsrfAwareActionInterface
                         $iyziCardModel->setData('card_user_key', $response->getCardUserKey());
                         $iyziCardModel->setData('api_key', $apiKey);
                         $iyziCardModel->save();
-                        $tableName = $resource->getTableName('iyzico_card');
-                        $connection->insert($tableName, $iyziCardModel->getData());
                     }
                 }
             }
